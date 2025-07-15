d4f3022817ed2483a7db98f5e7595c45dbf764bb
Altair Lage <altair.campos@watchguard.com>
2025-07-15 20:00:28 -0300

    Adiciona scripts de checkout e docker-compose

---
+++ pipeline.bat
@@ -0,0 +1,18 @@
+echo "Baixando os demais repositorios..."
+
+git clone https://github.com/altairlage/orderhub-core.git ../orderhub-core
+
+git clone https://github.com/altairlage/orderhub-cliente-service.git ../orderhub-cliente-service
+
+git clone https://github.com/altairlage/orderhub-produto-service.git ../orderhub-produto-service
+
+git clone https://github.com/altairlage/orderhub-estoque-service.git ../orderhub-estoque-service
+
+git clone https://github.com/altairlage/orderhub-pedido-receiver.git ../orderhub-pedido-receiver
+
+git clone https://github.com/altairlage/orderhub-pagamento-service.git ../orderhub-pagamento-service
+
+echo "Compilando os serviços com docker e rodando com docker-compose..."
+
+REM Build and run the services:
+docker-compose up --build
