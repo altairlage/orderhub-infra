3473ac46b2e0e7551cad64ed99b29b84fc9b7e6d
Vitor Cometti <vitor.cometti@ntconsult.com.br>
2025-07-30 18:32:50 -0300

    Adicao do pedido-service

--- infra-files/build-deploy-run-local.bat
+++ infra-files/build-deploy-run-local.bat
@@ -10,6 +10,8 @@ git clone https://github.com/altairlage/orderhub-estoque-service.git ../orderhub
 
 git clone https://github.com/altairlage/orderhub-pedido-receiver.git ../orderhub-pedido-receiver
 
+git clone https://github.com/altairlage/orderhub-pedido-service.git ../orderhub-pedido-service
+
 git clone https://github.com/altairlage/orderhub-pagamento-service.git ../orderhub-pagamento-service
 
 echo "Compilando os serviços com docker e rodando com docker-compose..."
--- infra-files/run-from-dockerhub.sh
+++ infra-files/run-from-dockerhub.sh
@@ -11,6 +11,8 @@ git clone https://github.com/altairlage/orderhub-estoque-service.git ../orderhub
 
 git clone https://github.com/altairlage/orderhub-pedido-receiver.git ../orderhub-pedido-receiver
 
+git clone https://github.com/altairlage/orderhub-pedido-service.git ../orderhub-pedido-service
+
 git clone https://github.com/altairlage/orderhub-pagamento-service.git ../orderhub-pagamento-service
 
 echo "*** Baixando as imagens..."
